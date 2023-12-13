560edce650a9cbf40439b28f204c727e05724475
Quang <107358125+Do-Ngoc-Quang@users.noreply.github.com>
2023-12-13 14:46:58 +0700

    commint

--- asset/doan_websitethuongmai.sql
+++ asset/doan_websitethuongmai.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 13, 2023 at 08:40 AM
+-- Generation Time: Dec 13, 2023 at 08:46 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
